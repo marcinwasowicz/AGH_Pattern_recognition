efd5cd04a7103e0365f80230b6977fdbd575da7a
marcinwasowicz <marcinkwasowicz@gmail.com>
2021-11-21 15:51:33 +0100

    Implement transfer learning with CIFAR 10 pretrained model.

--- transfer_learning/private_architecture/train_private_architecture.py
+++ transfer_learning/private_architecture/train_private_architecture.py
@@ -1,6 +1,8 @@
 import logging
 import pickle
 
+import tensorflow as tf
+
 from dataset import load_dataset, LASAGNE_MUSAKA_TART, DUMBELL_KETTLEBELL_BAREBELL
 from private_architecture import build_convolutional_architecture
 
@@ -8,6 +10,23 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("private architecture trainer")
 
 EPOCHS = 150
+
+
+def get_cifar10_pretrained(cifar10_path, num_classes=3):
+    model1 = tf.keras.models.load_model(cifar10_path)
+    for layer in model1.layers:
+        layer.trainable = False
+
+    new_dense_layer = tf.keras.layers.Dense(num_classes, activation="softmax")(
+        model1.layers[-2].output
+    )
+    model2 = tf.keras.models.Model(inputs=model1.input, outputs=new_dense_layer)
+    model2.compile(
+        optimizer=tf.keras.optimizers.SGD(learning_rate=0.001, momentum=0.9),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+        metrics=["acc"],
+    )
+    return model2
 
 
 def train_network(name, net, x, y):
@@ -23,7 +42,8 @@ if __name__ == "__main__":
         ("dumbell_kettlebell_barebell", load_dataset(DUMBELL_KETTLEBELL_BAREBELL)),
     ]:
         for network_name, network in [
-            ("directly_trained", build_convolutional_architecture())
+            ("directly_trained", build_convolutional_architecture()),
+            ("cifar10_pretrained", get_cifar10_pretrained("models/cifar10_trained")),
         ]:
             save_name = "_".join([dataset_name, network_name])
             logger.info(f" Started {save_name} training.")
